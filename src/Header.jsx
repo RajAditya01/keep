42995db37d1cd368576e2544e4b18a59a0c1f2a8
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-09-24 12:05:01 +0530

    .

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Header = () => {
+    return <>
+    <div className='header'>
+        <immg src="" alt="logo" />
+    </div>
+    </>
+};
+
+export default Header;
